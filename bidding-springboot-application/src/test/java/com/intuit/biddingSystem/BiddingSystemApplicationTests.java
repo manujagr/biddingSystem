4eed41e47af720216051deb2e2c5cb3a412c8d0f
manuj.agarwal <manujagarwal19997@gmail.com>
2024-10-30 10:27:15 +0530

    Moved integration testing file

--- bidding-springboot-application/src/test/java/com/intuit/biddingSystem/BiddingSystemApplicationTests.java
+++ bidding-springboot-application/src/test/java/com/intuit/biddingSystem/BiddingSystemApplicationTests.java
@@ -3,6 +3,8 @@ package com.intuit.biddingSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//todo - This class is supposed to used for integration testing,
+//empty as of now
 @SpringBootTest
 class BiddingSystemApplicationTests {
 
